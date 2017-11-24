57af1f2639d8efdb02e0622aeda1d69d0d5beff0
casterfile <dev.anthonycastor@gmail.com>
2017-11-24 11:42:11 +0800

    Upload

---
+++ Assets/_MyAsset/_Script/SceneControllerSlideShow.cs
@@ -0,0 +1,103 @@
+namespace GoogleVR.GVRDemo {
+  	using UnityEngine;
+	using System.Collections;
+	using UnityEngine.Video;
+	using UnityEngine.UI;
+
+	public class SceneControllerSlideShow : MonoBehaviour {
+		private  GameObject[] objVideo,objPicture;
+		private  VideoPlayer[] VideoPlayerControl;
+		private string[] VideoURL;
+		private int totalLoop = 0;
+		private int CurrentPicture = 0,CurrentVideo = 0;
+		private bool isVideo = false;
+
+	    void Start() {
+			int ArrayLength = 9;
+			totalLoop = ArrayLength - 1;
+			objVideo = new GameObject[ArrayLength];
+			objPicture = new GameObject[ArrayLength];
+			VideoURL = new string[ArrayLength];
+			VideoPlayerControl = new VideoPlayer[ArrayLength];
+			FindAllVideos ();
+			URLSetting ();
+			HideAllVideos ();
+//			StartCoroutine(Transition());
+//			objPicture [0].SetActive (true);
+			TransitionStart();
+	    }
+
+
+		private void HideAllVideos(){
+			for (int x = 0; x <= totalLoop; x++) {
+				objVideo [x].SetActive(false);
+				objPicture [x].SetActive(false);
+			}
+		}
+
+
+		private void FindAllVideos(){
+//			objVideo [0]=  GameObject.Find("360 Videos (0)");
+			for (int x = 0; x <= totalLoop; x++) {
+				print ("COunt: "+ x);
+				objVideo[x] =  GameObject.Find("VideoList/360 Videos ("+x+")");
+				VideoPlayerControl[x] =  objVideo[x].GetComponent<VideoPlayer>();
+				objPicture[x] =  GameObject.Find("PhotoList/360 Photo ("+x+")");
+
+
+//				objVideo [x].SetActive(false);
+//				objPicture [x].SetActive(false);
+			}
+
+	
+		}
+		private void TransitionStart(){
+			HideAllVideos ();
+			isVideo = !isVideo;
+			if (isVideo == true) {
+				if(CurrentVideo == totalLoop){
+					CurrentVideo = 0;
+				}
+				objVideo [CurrentPicture].SetActive (true);
+				CurrentVideo++;
+				StartCoroutine(Transition());
+				
+			} else {
+				if(CurrentPicture == totalLoop){
+					CurrentPicture = 0;
+				}
+				objPicture [CurrentPicture].SetActive (true);
+				CurrentPicture++;
+				StartCoroutine(Transition());
+			}
+
+		}
+
+
+		IEnumerator Transition()
+		{
+			yield return new WaitForSeconds(25);
+			TransitionStart ();
+		}
+
+
+		void URLSetting(){
+//			VideoURL[0] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[1] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[2] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[3] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[4] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[5] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[6] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[7] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//			VideoURL[8] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+//
+//			for (int x = 0; x <= totalLoop; x++) {
+//				VideoPlayerControl [x].source = VideoSource.Url;
+//				VideoPlayerControl [x].url = VideoURL[x];
+//			}
+		}
+
+  }
+}
+
